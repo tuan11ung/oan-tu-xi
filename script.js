81bb43578849dc8e5b6e038a8a7c31423d9bb6fa
Tuan11ung <nguyenduytuan11ung@gmail.com>
2026-01-06 16:27:21 +0700

    add button for user to choose their choice

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
 let humanScore = 0;
 let computerScore = 0;
 
+const playerOption = document.querySelector('.option')
+const hammerButton = document.querySelector('#hammer')
+const leafButton = document.querySelector('#leaf')
+const scissorsButotn = document.querySelector('#scissors')
+const result = document.querySelector('#result')
+
+const LEAF_OPTION = 'Leaf'
+const HAMMER_OPTION = 'Hammer'
+const SCISSORS_OPTION = 'Scissors'
+const WINNING_POINT = 5
+
+playerOption.addEventListener('click', (e) => {
+  let target = e.target
+  switch(target.id) {
+    case 'leaf':
+      playRound(LEAF_OPTION)
+      break;
+    case 'scissors':
+      playRound(SCISSORS_OPTION)
+      break;
+    case 'hammer':
+      playRound(HAMMER_OPTION)
+      break;
+  }
+})
+
+const checkWinner = (human, computer) => {
+  
+  const resultLog = document.createElement('p')
+  if (humanScore == 5) {
+    resultLog.textContent = `YOU ARE THE WINNER!!! ${humanChoice} beats ${computerChoice}, You ${humanScore} - ${computerScore} Com`
+    result.appendChild(resultLog)
+    humanScore = 0;
+    computerScore = 0;
+    const clearButton = document.createElement('button')
+    clearButton.textContent = 'Clear'
+    result.appendChild(clearButton)
+    clearButton.addEventListener('click', () => {
+      result.innerHTML = ''
+    })
+  }
+}
+
 function getRandomInt(num) {
   return Math.floor(Math.random() * num);
 }
@@ -8,54 +51,73 @@ function getRandomInt(num) {
 function getComputerChoice() {
   let computerChoice = getRandomInt(3)
   if (computerChoice == 0) {
-    return "Keo"
+    return SCISSORS_OPTION
   } else if (computerChoice == 1) {
-    return "Bua"
-  } else return "Bao"
+    return HAMMER_OPTION
+  } else return LEAF_OPTION
 }
 
-function getHumanChoice() {
-  let sign = prompt("What's your choice?")
-  sign.toLowerCase()
-  if (sign == "keo") return "Keo"
-  else if (sign == "bua") return "Bua"
-  else return "Bao"
+function getHumanChoice(option) {
+  if (option == SCISSORS_OPTION) return SCISSORS_OPTION
+  else if (option == HAMMER_OPTION) return HAMMER_OPTION
+  else return LEAF_OPTION
 }
 
-function playRound() {
-  let humanChoice = getHumanChoice()
+function playRound(option) {
+  let humanChoice = getHumanChoice(option)
   let computerChoice = getComputerChoice()
-  if (humanChoice === computerChoice) return console.log(`Hoa, ${humanScore} - ${computerScore}`)
-  if (humanChoice == "Bua" && computerChoice == "Keo" || humanChoice == "Keo" && computerChoice == "Bao" || humanChoice == "Bao" && computerChoice == "Bua") {
-    humanScore++
-    return console.log(`Thang roi! ${humanChoice} thang ${computerChoice}, ${humanScore} - ${computerScore}`)
+
+  const resultLog = document.createElement('p')
+
+  if (humanChoice === computerChoice) {
+    console.log(`DRAW, ${humanChoice} = ${computerChoice}, You ${humanScore} - ${computerScore} Com`)
+    resultLog.textContent = `DRAW, ${humanChoice} = ${computerChoice}, You ${humanScore} - ${computerScore} Com`
+    return result.appendChild(resultLog)
   }
+
+  if (humanChoice == HAMMER_OPTION && computerChoice == SCISSORS_OPTION || humanChoice == SCISSORS_OPTION && computerChoice == LEAF_OPTION || humanChoice == LEAF_OPTION && computerChoice == HAMMER_OPTION) {
+    humanScore++
+    console.log(`WIN! ${humanChoice} beats ${computerChoice}, You ${humanScore} - ${computerScore} Com`)
+    resultLog.textContent = `WIN! ${humanChoice} beats ${computerChoice}, You ${humanScore} - ${computerScore} Com`
+    result.appendChild(resultLog)
+    if (humanScore == WINNING_POINT) {
+      resultLog.textContent = `YOU ARE THE WINNER!!! ${humanChoice} beats ${computerChoice}, You ${humanScore} - ${computerScore} Com`
+      result.appendChild(resultLog)
+      humanScore = 0;
+      computerScore = 0;
+      const clearButton = document.createElement('button')
+      clearButton.textContent = 'Clear'
+      result.appendChild(clearButton)
+      clearButton.addEventListener('click', () => {
+        result.innerHTML = ''
+      })
+    }
+  }
+
   else {
     computerScore++
-    return console.log(`Thua roi! ${computerChoice} thang ${humanChoice}, ${humanScore} - ${computerScore}` )
+    console.log(`LOSE! ${computerChoice} beats ${humanChoice}, You ${humanScore} - ${computerScore} Com` )
+    resultLog.textContent = `LOSE! ${computerChoice} beats ${humanChoice}, You ${humanScore} - ${computerScore} Com`
+    result.appendChild(resultLog)
+    if (computerScore == WINNING_POINT) {
+      resultLog.textContent = `you are loser... ${humanChoice} beats ${computerChoice}, You ${humanScore} - ${computerScore} Com`
+      result.appendChild(resultLog)
+      humanScore = 0;
+      computerScore = 0;
+      const clearButton = document.createElement('button')
+      clearButton.textContent = 'Clear'
+      result.appendChild(clearButton)
+      clearButton.addEventListener('click', () => {
+        result.innerHTML = ''
+      })
+    }
   }
+
+
 }
 
-function playGame() {
-  while (humanScore < 5 && computerScore < 5) {
-    playRound();
-    console.log(`${humanScore} - ${computerScore}`)
-  }
-  if (humanScore == 5) {
-    humanScore = 0;
-    computerScore = 0;
-    console.log("Xin chuc mung ban da thang!!!")
-    playGame()
-  } 
-  else {
-    humanScore = 0;
-    computerScore = 0;
-    console.log("Xin chia buon ban da thua!!!")
-    playGame()
-  } 
-}
 
-playGame()
+
 
 
 
